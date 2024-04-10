d94c90a4e3de39df5d88b8bfd10fada0fca38c3f
Tomás Bizet <tbizetde@gmail.com>
2024-04-10 12:57:06 -0300

    st, navbar

---
+++ components/Layouts/DefaultNavBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const router = useRouter()
+
+const items = [
+  {
+    title: 'about',
+    link: '/about'
+  },
+  {
+    title: 'web3',
+    link: '/web3'
+  },
+  {
+    title: 'methods',
+    link: '/methods'
+  }
+]
+</script>
+
+<template>
+  <div
+    class="flex w-full h-40 bg-black text-white gap-4 text-2xl items-end justify-between px-40"
+  >
+    <div @click="router.push('/')" class="text-4xl">S.E.E.D.</div>
+    <div class="flex gap-4 flex-row">
+      <div v-for="(item, index) in items" key="index" class="flex gap-4">
+        <div v-if="index > 0">｜</div>
+        <div @click="router.push(item.link)">{{ item.title }}</div>
+      </div>
+    </div>
+    <div @click="router.push('/')">
+      <img src="@/assets/img/logo_singularity.png" class="w-40" />
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,3 @@
+<template>
+  <div><LayoutsDefaultNavBar></LayoutsDefaultNavBar></div>
+</template>
