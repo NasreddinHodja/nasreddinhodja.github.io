89630914250d61ce120d14b9332ed497c5416a6a
tbizetde@gmail.com <tbizetde@gmail.com>
2025-03-10 14:25:01 -0300

    rm welcome card

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,7 @@
     class="w-full flex flex-col items-center bg-[url('/public/bg.png')] bg-cover bg-center bg-fixed md:py-24 py-8 px-2"
   >
     <div class="md:w-2/3 w-full flex flex-col md:gap-10 gap-4">
-      <div class="flex xl:flex-row flex-col md:gap-10 gap-4">
-        <WelcomeCard />
-        <ProfileCard />
-      </div>
+      <ProfileCard />
       <PiecesList />
     </div>
   </div>
