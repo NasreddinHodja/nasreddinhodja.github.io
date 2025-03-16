fc365d4be41527a3fd882584d644c2014bfa7397
tbizetde@gmail.com <tbizetde@gmail.com>
2025-03-16 16:35:35 -0300

    layout

--- components/BaseCard.vue
+++ components/BaseCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col md:gap-10 gap-4 bg-black/40 backdrop-blur-2xl justify-center md:p-10 p-6 rounded-xl shadow-md md:shadow-xl"
+    class="flex flex-col md:gap-10 gap-4 bg-black/90 justify-center md:p-10 p-6 shadow-md md:shadow-xl"
   >
     <slot />
   </div>
